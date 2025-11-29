74027b08c8a0dc6fd897cac639a746a91e90c293
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-11-29 04:49:05 +0100

    checking out file issues still

---
+++ app/syncfusion.client.ts
@@ -0,0 +1,3 @@
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+        tailwindcss(),
+        reactRouter(), // ✔ No arguments allowed
+        tsconfigPaths(),
+    ],
+
     ssr: {
-      noExternal: [/@syncfusion/],
-    }
+        noExternal: [/@syncfusion/],
+    },
 });
